93762f6bbb273726b29dcecac06f58f3e2ad4437
jhosbertjt <82987202+jhosbertjt@users.noreply.github.com>
2022-09-06 21:36:44 -0500

    Cambios desde IntelliJ

--- Test-project/src/Main.java
+++ Test-project/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Este mensaje es de prueba");
+        System.out.println("Este mensaje es de prueba modificado desde intelliJ");
     }
 }
