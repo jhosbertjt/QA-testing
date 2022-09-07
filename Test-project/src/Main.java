17b8b8a04dbb77dbe51c48ec694d8cee3c3c9d18
jhosbertjt <82987202+jhosbertjt@users.noreply.github.com>
2022-09-06 21:38:55 -0500

    Cambios desde IntelliJ

--- Test-project/src/Main.java
+++ Test-project/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Este mensaje es de prueba");
         System.out.println("Este mensaje es de prueba modificado desde intelliJ");
+        System.out.println("Este mensaje es de prueba modificado desde intelliJ2");
         System.out.println("Este mensaje es de prueba modificado desde servidor");
     }
 }
